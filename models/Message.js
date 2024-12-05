9aa96d8968e33a3747b7c9876879c9c617962e90
Naufal Yoga Pratama <naufaltino2113@gmail.com>
2024-12-06 01:35:53 +0700

    one way chat

--- models/Message.js
+++ models/Message.js
@@ -1,11 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const messageSchema = new mongoose.Schema({
-  sender: { type: String, required: true },
-  message: { type: String, required: true },
-  timestamp: { type: Date, default: Date.now },
+const MessageSchema = new mongoose.Schema({
+  sender: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now, // Secara otomatis memberikan timestamp
+  },
 });
 
-const Message = mongoose.model('Message', messageSchema);
-
-module.exports = Message;
+module.exports = mongoose.model("Message", MessageSchema);
